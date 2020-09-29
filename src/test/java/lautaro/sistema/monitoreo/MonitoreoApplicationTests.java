95b90d527bc2a871dad07434d0719b717196df0d
Lautaro Garcia <lautygcr@gmail.com>
2020-09-29 01:35:02 -0300

    Commit Inicial

---
+++ src/test/java/lautaro/sistema/monitoreo/MonitoreoApplicationTests.java
@@ -0,0 +1,13 @@
+package lautaro.sistema.monitoreo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
