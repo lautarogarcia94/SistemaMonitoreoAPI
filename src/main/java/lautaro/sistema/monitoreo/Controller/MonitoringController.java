4baf4e67aa3d1977a486665a2637048345ecd7e5
Lautaro Garcia <lautygcr@gmail.com>
2020-09-29 01:48:46 -0300

    Se agrega clase MonitoringController

---
+++ src/main/java/lautaro/sistema/monitoreo/Controller/MonitoringController.java
@@ -0,0 +1,22 @@
+package lautaro.sistema.monitoreo.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("monitoring")
+public class MonitoringController {
+
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<String> createUser(@RequestBody Integer medicion) {
+
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
