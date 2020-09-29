f3059ec11ec079e305c8b97a4d184b810b8d8094
Lautaro Garcia <lautygcr@gmail.com>
2020-09-29 01:59:57 -0300

    Se renombra clase MonitoringRestController

--- src/main/java/lautaro/sistema/monitoreo/Controller/MonitoringRestController.java
+++ src/main/java/lautaro/sistema/monitoreo/Controller/MonitoringRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("monitoring")
-public class MonitoringController {
+public class MonitoringRestController {
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
