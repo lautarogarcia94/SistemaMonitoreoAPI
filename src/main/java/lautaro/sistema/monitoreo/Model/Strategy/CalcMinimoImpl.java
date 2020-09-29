d6eac9b3153af9c2254d1c27395946ab32253c2b
Lautaro Garcia <lautygcr@gmail.com>
2020-09-29 12:47:09 -0300

    Se agrega clase CalcMinimoImpl

---
+++ src/main/java/lautaro/sistema/monitoreo/Model/Strategy/CalcMinimoImpl.java
@@ -0,0 +1,30 @@
+package lautaro.sistema.monitoreo.Model.Strategy;
+
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Queue;
+
+@Component
+public class CalcMinimoImpl implements CalculoInt {
+
+    private final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CalcMinimoImpl.class);
+
+    @Override
+    public double calcular(Queue<Integer> cola) {
+
+        if (cola.size() == 0) {
+            LOGGER.warn("No hay elementos en la cola para calcular el promedio");
+            return 0.0;
+        }
+
+        int num = cola.peek();
+        for (Integer x : cola) {
+            if (num > x) {
+                num = x;
+            }
+        }
+        return num;
+    }
+}
